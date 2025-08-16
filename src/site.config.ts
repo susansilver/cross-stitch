b412795b81ef4c46563bd06d56641913fe67a815
suzza <suzza@susansilver.net>
2025-08-16 14:37:50 -0700

    add flosstube links

--- src/site.config.ts
+++ src/site.config.ts
@@ -48,7 +48,7 @@ export const theme: ThemeUserConfig = {
   header: {
     menu: [
       { title: 'Blog', link: '/blog' },
-      { title: 'Links', link: '/links' },
+      { title: 'Currently watching', link: '/links' },
       { title: 'About', link: '/about' }
     ]
   },
