7f55a395ea383e0962f60a22b978c7df09565a81
suzza <suzza@susansilver.net>
2025-08-17 20:04:57 -0700

    update astro config file

--- astro.config.ts
+++ astro.config.ts
@@ -25,7 +25,7 @@ import config from './src/site.config.ts'
 // https://astro.build/config
 export default defineConfig({
   // Top-Level Options
-  site: 'https://astro-pure.js.org',
+  site: 'https://suzzastitches.com',
   // base: '/docs',
   trailingSlash: 'never',
 
